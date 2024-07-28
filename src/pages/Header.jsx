6c392ace0ca2724aedf531c1b60d98e5c7586328
tejasnasre <tejasnasre120@gmail.com>
2024-07-28 20:20:54 +0530

    fix:ui

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,32 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <div className="bg-[#111111] w-full text-[#e7e7e7] py-10">
-        <h1 className="text-center py-4 px-2 md:px-0 text-[1.2rem] md:text-[2rem] font-bold">
-          &lt; &lt; &lt; TEJAS NASRE &gt; &gt; &gt;
-        </h1>
-        <div className="w-full flex flex-row justify-center gap-10 my-10">
-          <Link to="/" className="border border-white rounded-lg px-4 py-2 flex justify-center items-center">
+      <div className="bg-[#111111] w-full text-[#e7e7e7] pt-10 relative">
+        <div
+          className="px-5 flex justify-end cursor-pointer md:hidden"
+          onClick={() => {
+            return setOpen(!open);
+          }}
+        >
+          {open ? (
+            <h1 className="flex text-black z-[999] text-xl">
+              &lt;&lt;&lt;&lt;
+            </h1>
+          ) : (
+            <h1 className="text-xl">&gt;&gt;&gt;&gt;</h1>
+          )}
+        </div>
+        <div
+          className={`w-full ${
+            open
+              ? `flex absolute h-screen top-0 bg-white text-black flex-col justify-center items-center overflow-hidden`
+              : "hidden"
+          } md:flex flex-col gap-5 md:flex-row md:justify-center md:gap-10`}
+        >
+          <Link
+            to="/"
+            className="border border-white rounded-lg text-[1rem] md:px-4 py-2 flex justify-center items-center"
+          >
             Home
           </Link>
           <Link
             to="/about"
-            className="border border-white rounded-lg px-4 py-2 flex justify-center items-center"
+            className="border border-white rounded-lg text-[1rem] md:px-4 py-2 flex justify-center items-center"
           >
             About Me
           </Link>
           <Link
             to="/experience"
-            className="border border-white rounded-lg px-4 py-2 flex justify-center items-center"
+            className="border border-white rounded-lg text-[1rem] md:px-4 py-2 flex justify-center items-center"
           >
             Experience
           </Link>
           <Link
             to="/contact"
-            className="border border-white rounded-lg px-4 py-2 flex justify-center items-center"
+            className="border border-white rounded-lg text-[1rem] md:px-4 py-2 flex justify-center items-center"
           >
             Contact
           </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,34 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="bg-[#111111] w-full text-[#e7e7e7] h-screen">
-      <h1 className="text-center text-5xl font-extrabold">
+    <div className="bg-[#111111] w-full text-[#e7e7e7] h-screen flex flex-col justify-start items-center gap-24 md:gap-32">
+      <h1 className="text-center py-4 px-2 md:px-0 text-[1.2rem] md:text-[2rem] font-bold mt-10">
+        &lt; &lt; &lt; TEJAS NASRE &gt; &gt; &gt;
+      </h1>
+      <h1 className="text-center text-5xl font-extrabold px-2">
         WELCOME TO MY PORTFOLIO
       </h1>
+      <div class="w-full">
+        <div class="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+          <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+            <li>JAVASCRIPT</li>
+            <li>TYPESCRIPT</li>
+            <li>PYTHON</li>
+            <li>NEXT JS</li>
+            <li>REACT JS</li>
+          </ul>
+          <ul
+            class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+            aria-hidden="true"
+          >
+            <li>JAVASCRIPT</li>
+            <li>TYPESCRIPT</li>
+            <li>PYTHON</li>
+            <li>NEXT JS</li>
+            <li>REACT JS</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
